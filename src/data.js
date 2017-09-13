3a5a8708ca81c430a6dc16c11494430324cdf00d
M Coindreau <mariannacoindreau@gmail.com>
2017-09-13 12:09:36 -0500

    includes modules for require

---
+++ src/data.js
@@ -0,0 +1,9 @@
+const data = {
+    someNumber: 42,
+    whichSideOfTheForce: 'The Dark Side',
+    arnie: 'Get to da choppa!',
+};
+
+//json notation assigned to data
+
+module.exports = data;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const $ = require("jquery"); // loading jquery.min.js
+const data = require('./data');
+const math = require('./math');
+
+console.log(math.add(10, 98));
+console.log(math.divide(10, 2.5));
+console.log(math.multiply(4, 5)); // logs `9`
+console.log(math.subtract(4, 2)); // logs `2`
+
+//const $ = require("jquery"); // loading jquery.min.js
 
 const hello = () => console.log("hello Redwoods from webkpack!");
 
 hello();
+console.log(data.whichSideOfTheForce); // outputs "The Dark Side"
 
 $("body")
     .html("<h1>Hello from jQuery</h1>")
-    .css("background-color", "lightgray")
+    .css("background-color", "lightgray");
