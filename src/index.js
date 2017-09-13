092c5daaf93995d0417a057c2ec45a32e0492c78
M Coindreau <mariannacoindreau@gmail.com>
2017-09-13 11:34:06 -0500

    install jQuery

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+const $ = require("jquery"); // loading jquery.min.js
+
 const hello = () => console.log("hello Redwoods from webkpack!");
 
 hello();
+
+$("body")
+    .html("<h1>Hello from jQuery</h1>")
+    .css("background-color", "lightgray")
